2a14c29876c770c580cc1f8597010f09e6fffb47
Olivier Mignault <olivier@orb.net>
2025-05-09 14:29:38 -0400

    make MQTT feature configurable (#4)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,16 @@ echo "Starting MQTT publishing script..."
 date
 
 # Start MQTT publishing in the background
-/app/mqtt.sh &
+CONFIG_PATH=/data/options.json
+MQTT_PUSH=$(jq -r '.mqtt_push // false' $CONFIG_PATH)
+
+if [ "$MQTT_PUSH" == "true" ]; then
+  echo "MQTT Push is enabled"
+  /app/mqtt.sh &
+else
+  echo "MQTT Push is disabled"
+fi
+
 
 # Now start the original entrypoint or command
 exec /app/orb sensor
