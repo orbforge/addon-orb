612134d4c6ca248dc3d945f69642d4574a9f9892
Olivier Mignault <olivier@orb.net>
2026-07-07 10:31:46 -0400

    configurable ephemeral mode (#23)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,6 +7,19 @@ date
 
 # Start MQTT publishing in the background
 CONFIG_PATH=/data/options.json
+if [ -n "$ORB_EPHEMERAL_MODE" ]; then
+  echo "Using ORB_EPHEMERAL_MODE from environment variables"
+else
+  EPHEMERAL_MODE=$(jq -r '.ephemeral_mode // false' "$CONFIG_PATH")
+  if [ "$EPHEMERAL_MODE" = "true" ]; then
+    ORB_EPHEMERAL_MODE="1"
+    export ORB_EPHEMERAL_MODE
+    echo "ORB_EPHEMERAL_MODE is enabled"
+  else
+    echo "ORB_EPHEMERAL_MODE is disabled"
+  fi
+fi
+
 if [ -n "$MQTT_PUSH" ]; then
   echo "Using MQTT_PUSH from environment variables"
 else
